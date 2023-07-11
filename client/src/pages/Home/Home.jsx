f9d6b732943732b2b4134c1a87b078bc563f3fdb
Shangguan Wang <wangshangguan@hotmail.com>
2023-07-11 10:54:07 -0500

    Func: Categories component added

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,12 +4,14 @@ import "./Home.scss"
 
 import Slider from '../../components/Slider/Slider'
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
+import Categories from '../../components/Categories/Categories'
 
 const Home = () => {
   return (
     <div className='home'>
       <Slider />
       <FeaturedProducts type="featured"/>
+      <Categories />
       <FeaturedProducts type="trending"/>
     </div>
   )
